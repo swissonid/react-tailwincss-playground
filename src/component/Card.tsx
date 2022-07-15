92e06b76590d8c461971720b56d86a374554b158
Patrice Müller <mueller.patrice@gmail.com>
2022-07-15 11:26:18 +0200

    Just made setup

---
+++ src/component/Card.tsx
@@ -0,0 +1,13 @@
+function Card() {
+  return (
+    <>
+      <div className="bg-slate-300 p-5 rounded-md shadow-md">
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+        <p>Just a text</p>
+      </div>
+    </>
+  )
+}
+
+
+export default Card;
